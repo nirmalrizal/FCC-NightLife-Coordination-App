22bd0f0bd146a57016856a17f0cf1a021f6ded52
Nirmal Rijal <nirmalrijal41@gmail.com>
2017-05-25 06:03:58 +0545

    Custom Search and Bootstrap Added

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,28 @@ var yelp = require('yelp-fusion');
 
 var clientId = '5vg4arEfVEd-6ZHKgZa1oQ';
 var clientSecret = 'UNj216lADVN32XhsPkArizKZfvzhDtaaYSNLonHDJcFLMxXCICTKRKHSMYED5Nje';
-var searchRequest = {
-					  term:'bars',
-					  location: 'london'
-					};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	res.render('index');
+});
+
+router.post('/search',function(req, res){
+	var loc = req.body.location;
+	console.log(loc);
+	var searchRequest = {
+					  term:'bars',
+					  location: loc
+					};
+	console.log(searchRequest);
 	yelp.accessToken(clientId, clientSecret).then(function(res){
 		var client = yelp.client(res.jsonBody.access_token);
 
 		client.search(searchRequest).then(function(res){
 		    var firstResult = res.jsonBody.businesses[1];
 		    var prettyJson = JSON.stringify(firstResult, null, 4);
-		    console.log(res.jsonBody.businesses.length); //this is actual data
+		    console.log(firstResult); //this is actual data
+		    res.render('index');
 		  });
 		}).catch(function(err){
 		  console.log(err);
